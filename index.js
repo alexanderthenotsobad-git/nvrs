63a073453bfa8d676bcbc55603acfa8286ba96d0
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-09-14 02:20:36 +0000

    Troubleshooting output type for getAllItems

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import appRouter from "./routes/index.js";
 import { config } from "dotenv";
 import { connectToDatabase } from "./db/index.js";
 const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: 'http://localhost:8787', // Allow only your frontend origin
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
  
 config(); // Load environment variables 
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const appRouter = Router();
 
 appRouter.get("/", getAllItems); // api/items => [...]
 appRouter.get("/:id",getItem) // api/items/[id] (eg. id = 42)
-appRouter.post("/create", createItem) // api/items/create[id] (eg. post new product id = 42)
+appRouter.post("/create", createItem) // api/items/create (eg. post new product id = 42)
 appRouter.put("/update/:id", updateItem) // api/items/update[id] (eg. put (update) existing product)
 appRouter.delete("/delete/:id", deleteItem) // api/items/delete[id] (eg. delete existing product)
 
