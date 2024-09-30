a8e0cb3b3e6733869cff229c73ca9a1cecc58be9
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-09-30 01:37:39 +0000

    Fixed Add-Items

--- handlers/index.js
+++ handlers/index.js
@@ -5,7 +5,7 @@ export const getAllItems = async(req,res) => {
         const item = await find();
         return res.status(200).json({ item });
         // Log the return item to the console
-        console.log('Fetched items:', JSON.stringify(items, null, 2));
+        // Console.log('Fetched items:', JSON.stringify(item, null, 2));
      }catch (error) {
         console.log(`Error getting items: ${error}`);
         res.status(500).json({ message: "Error" });
@@ -16,14 +16,14 @@ export const getItem = async(req,res) => {};
 export const createItem = async(req,res) => {
    const { item_name, item_desc, price, item_pic } = req.body;
 
-   if (!item_name || !item_desc || !price) {
-      return res
-      .status(403)
-      .json({message: "Cannot create. Please enter item name desc and price."});
-      }  
+   // if (!item_name || !item_desc || !price) {
+   //    return res
+   //    .status(403)
+   //    .json({message: "Cannot create. Please enter item_name item_desc and price."});
+   //    }  
       try {
-         const item = await create(item_name, item_desc, price);
-         return res.status(201).json({ item })
+         const items = await create(item_name, item_desc, price, item_pic);
+         return res.status(201).json({ items });
       } catch (error) {
          console.log(`Error inserting item: ${error}`);
          res.status(500).json({ message: "Error"});
